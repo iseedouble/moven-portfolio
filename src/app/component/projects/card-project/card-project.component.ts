13dea280e75c2704f7356a92b03d2a772c193363
iseedouble <tsteven@live.ca>
2024-12-04 23:57:19 -0500

    fix animation

--- src/app/component/projects/card-project/card-project.component.ts
+++ src/app/component/projects/card-project/card-project.component.ts
@@ -1,20 +1,53 @@
 import { Component, Input } from '@angular/core';
 import { Position } from '../../../model/position';
 import { CommonModule } from '@angular/common';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  keyframes,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-card-project',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './card-project.component.html',
-  styleUrl: './card-project.component.scss'
+  styleUrl: './card-project.component.scss',
+  animations: [
+    trigger('minimizeAndDisappear', [
+      // Fade-in animation
+      transition(':enter', [
+        animate(
+          '300ms ease-out',
+          keyframes([
+            style({ transform: 'scale(0)', opacity: 0, offset: 0 }), // Fully minimized and invisible
+            style({ transform: 'scale(0.5)', opacity: 0.5, offset: 0.5 }), // Grow halfway
+            style({ transform: 'scale(1)', opacity: 1, offset: 1 }), // Fully grown and visible
+          ])
+        ),
+      ]),
+      // Minimize and disappear animation
+      transition(':leave', [
+        animate(
+          '300ms ease-in',
+          keyframes([
+            style({ transform: 'scale(1)', opacity: 1, offset: 0 }), // Start at full size
+            style({ transform: 'scale(0.5)', opacity: 0.5, offset: 0.5 }), // Shrink and fade halfway
+            style({ transform: 'scale(0)', opacity: 0, offset: 1 }), // Fully minimized and invisible
+          ])
+        ),
+      ]),
+    ]),
+  ],
 })
 export class CardProjectComponent {
-
   @Input() img!: string;
   @Input() title!: string;
   @Input() description!: string;
   @Input() position!: Position;
+  @Input() activeButton!: string;
 
   cardProject = [
     {
@@ -67,4 +100,12 @@ export class CardProjectComponent {
     },
   ];
 
+  public projectMap: Map<string, string[]> = new Map([
+    ['All', ['project0', 'project1', 'project2', 'project3']],
+    ['Web Design', ['project0', 'project1', 'project2']],
+    ['Graphics', ['project0', 'project2']],
+    ['App', ['project0', 'project3']],
+  ]);
+
+  constructor() {}
 }
--- src/app/component/projects/projects.component.ts
+++ src/app/component/projects/projects.component.ts
@@ -1,40 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { CardProjectComponent } from './card-project/card-project.component';
 import { CommonModule } from '@angular/common'; // Import NgIf
-import { trigger, transition, style, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
   imports: [CardProjectComponent, CommonModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss',
-  animations: [
-    trigger('minimizeAndDisappear', [
-      // Fade-in animation
-      transition(':enter', [
-        animate(
-          '300ms ease-out',
-          keyframes([
-            style({ transform: 'scale(0)', opacity: 0, offset: 0 }), // Fully minimized and invisible
-            style({ transform: 'scale(0.5)', opacity: 0.5, offset: 0.5 }), // Grow halfway
-            style({ transform: 'scale(1)', opacity: 1, offset: 1 }) // Fully grown and visible
-          ])
-        )
-      ]),
-      // Minimize and disappear animation
-      transition(':leave', [
-        animate(
-          '300ms ease-in',
-          keyframes([
-            style({ transform: 'scale(1)', opacity: 1, offset: 0 }), // Start at full size
-            style({ transform: 'scale(0.5)', opacity: 0.5, offset: 0.5 }), // Shrink and fade halfway
-            style({ transform: 'scale(0)', opacity: 0, offset: 1 }) // Fully minimized and invisible
-          ])
-        )
-      ])
-    ])
-  ]
+  styleUrl: './projects.component.scss'
+
 })
 export class ProjectsComponent {
   activeButton = 'All'; // Default active button
